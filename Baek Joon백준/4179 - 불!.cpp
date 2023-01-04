4a00af027c80ff76e1aab47687ece301851f9c50
jimi567 <jimi567@kookmin.ac.kr>
2023-01-04 15:34:05 +0900

    T : x 3시간 넘게품 T : r 더 나은 풀이방식

--- Baek Joon백준/4179 - 불!.cpp
+++ Baek Joon백준/4179 - 불!.cpp
@@ -1,3 +1,5 @@
+//내가 푼 방식
+//불 bfs 지훈 bfs 실행 후 지훈이의 최단탈출 좌표를 따로 
 #include <bits/stdc++.h>
 using namespace std;
 int dx[4] = { 1,0,-1,0 };
@@ -133,4 +135,133 @@ int main() {
 
 	if (canEs) cout << viaJ[J.first][J.second];
 	else cout << "IMPOSSIBLE";
+}
+//좋은 풀이 방식
+// 각 큐에 지훈,불의 시작지점을 동시에 받아내고 
+// 불의 bfs 실행후 지훈이의 bfs중 불의 전파속도보다 작은 곳만 이동하게 추가 조건문을 넣음, 그리고 만약 px py가 범위를 탈출했을 경우는 
+// 탈출 성공한것이므로 바로 결과를 출력해주고 메인함수를 끝냄, 만약  지훈이 bfs가 다끝나도 메인함수가 끝나지않는다는건 탈출불가라는 뜻
+#include <bits/stdc++.h>
+using namespace std;
+string arr[1005]; // 미로 배열
+int dx[4] = { 1,0,-1,0 };
+int dy[4] = { 0,1,0,-1 };
+int viaJ[1005][1005]; // 지훈이의 이동 시간
+int viaF[1005][1005]; // 불의 전파 시간
+int main(void) {
+	ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+	int r, c;
+	cin >> r >> c;
+	//맵의 정보 입력받기
+	for (int i = 0; i < r; i++) {
+		cin >> arr[i];
+	}
+	queue <pair<int, int>> q1; // 지훈이 bfs용 큐
+	queue <pair<int, int>>  q2;// 불 bfs용 큐
+	//각 큐에 지훈이와 불의 시작지점 푸쉬하고 via에 표시
+	for (int i = 0; i < r; i++) {
+		for (int j = 0; j < c; j++) {
+			if (arr[i][j] == 'J') {
+				q1.push({ i,j });
+				viaJ[i][j] = 1;
+			}
+			if (arr[i][j] == 'F') {
+				q2.push({ i,j });
+				viaF[i][j] = 1;
+			}
+		}
+	}
+	//불의 bfs
+	while (!q2.empty()) {
+		auto cur = q2.front(); q2.pop();
+		for (int i = 0; i < 4; i++) {
+			int px = cur.first + dx[i];
+			int py = cur.second + dy[i];
+			if (px < 0 || px >= r || py < 0 || py >= c)continue;
+			if (viaF[px][py] != 0 || arr[px][py] != '.')continue; //이미 방문했거나 .이 아닌경우( 지날수없는 벽일 경우)
+			q2.push({ px,py });
+			viaF[px][py] = viaF[cur.first][cur.second] + 1;
+		}
+	}
+	//지훈 bfs
+	while (!q1.empty()) {
+		auto cur = q1.front(); q1.pop();
+		for (int i = 0; i < 4; i++) {
+			int px = cur.first + dx[i];
+			int py = cur.second + dy[i];
+			if (px < 0 || px >= r || py < 0 || py >= c) {//범위를 벗어나면 탈출 성공
+				cout << viaJ[cur.first][cur.second];
+				return 0;
+			}
+			if (viaJ[px][py] != 0 || arr[px][py] != '.') continue;
+			if (viaF[px][py] != 0 && viaJ[cur.first][cur.second] + 1 >= viaF[px][py]) continue; //지훈이의 도달 시간이 불의 전파속도보다 크거나 같으면 못지나가는곳임
+			q1.push({ px,py });
+			viaJ[px][py] = viaJ[cur.first][cur.second] + 1;
+
+		}
+	}
+	cout << "IMPOSSIBLE"; //탈출 성공을 못하고 bfs가 끝나면 탈출못하는 미로
+}
+
+#include <bits/stdc++.h>
+using namespace std;
+string arr[1005]; // 미로 배열
+int dx[4] = { 1,0,-1,0 };
+int dy[4] = { 0,1,0,-1 };
+int viaJ[1005][1005]; // 지훈이의 이동 시간
+int viaF[1005][1005]; // 불의 전파 시간
+int main(void) {
+	ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+	int r, c;
+	cin >> r >> c;
+	//맵의 정보 입력받기
+	for (int i = 0; i < r; i++) {
+		cin >> arr[i];
+	}
+	queue <pair<int, int>> q1; // 지훈이 bfs용 큐
+	queue <pair<int, int>>  q2;// 불 bfs용 큐
+	//각 큐에 지훈이와 불의 시작지점 푸쉬하고 via에 표시
+	for (int i = 0; i < r; i++) {
+		for (int j = 0; j < c; j++) {
+			if (arr[i][j] == 'J') {
+				q1.push({ i,j });
+				viaJ[i][j] = 1;
+			}
+			if (arr[i][j] == 'F') {
+				q2.push({ i,j });
+				viaF[i][j] = 1;
+			}
+		}
+	}
+	//불의 bfs
+	while (!q2.empty()) {
+		auto cur = q2.front(); q2.pop();
+		for (int i = 0; i < 4; i++) {
+			int px = cur.first + dx[i];
+			int py = cur.second + dy[i];
+			if (px < 0 || px >= r || py < 0 || py >= c)continue;
+			if (viaF[px][py] != 0 || arr[px][py] != '.')continue; //이미 방문했거나 .이 아닌경우( 지날수없는 벽일 경우)
+			q2.push({ px,py });
+			viaF[px][py] = viaF[cur.first][cur.second] + 1;
+		}
+	}
+	//지훈 bfs
+	while (!q1.empty()) {
+		auto cur = q1.front(); q1.pop();
+		for (int i = 0; i < 4; i++) {
+			int px = cur.first + dx[i];
+			int py = cur.second + dy[i];
+			if (px < 0 || px >= r || py < 0 || py >= c) {//범위를 벗어나면 탈출 성공
+				cout << viaJ[cur.first][cur.second];
+				return 0;
+			}
+			if (viaJ[px][py] != 0 || arr[px][py] != '.') continue;
+			if (viaF[px][py] != 0 && viaJ[cur.first][cur.second] + 1 >= viaF[px][py]) continue; //지훈이의 도달 시간이 불의 전파속도보다 크거나 같으면 못지나가는곳임
+			q1.push({ px,py });
+			viaJ[px][py] = viaJ[cur.first][cur.second] + 1;
+
+		}
+	}
+	cout << "IMPOSSIBLE"; //탈출 성공을 못하고 bfs가 끝나면 탈출못하는 미로
 }
