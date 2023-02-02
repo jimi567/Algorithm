35be12bdae435a81fdcd60f92d4677e7c280b676
KB Kim <jimi567@kookmin.ac.kr>
2023-02-03 01:11:02 +0900

    T : x 스케줄링알고리즘 익숙해져야됨 다시풀자

---
+++ Baek Joon백준/그리디/1931 - 회의실배정.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    static int n;
+
+    static int[][] task ;
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        task = new int[n][2];
+        for(int i=0;i<n;i++){
+            st = new StringTokenizer(br.readLine());
+            task[i][0] = Integer.parseInt(st.nextToken());
+            task[i][1] = Integer.parseInt(st.nextToken());
+        }
+    }
+
+    static void solution(){
+        //종료 시간 기준 정렬하되, 시작하자마 끝나는 예외로 인해 종료시간이 같으면 시작시간 기준으로 종료
+        //ex (2,2) (1,2) 첫번째가 먼저오게되면 2개의 task를 처리할수있음에도 하나의 task만 처리하게됨
+        Arrays.sort(task, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[1] == o2[1]) return o1[0] - o2[0]; //종료시간이 같으면 시작시간이 빠른거
+                return o1[1] - o2[1]; //종료시간이 빠른거
+            }
+        });
+
+        int ret = 0;
+        int t = 0; //이전 회의 종료시간
+        for(int i=0;i<n;i++){
+            if(t > task[i][0]) continue; //회의 시작시간보다 이전 회의 종료시간이 크면 갱신x
+            t = task[i][1];
+            ret++;
+        }
+        System.out.println(ret);
+    }
+    public static void main(String[] args) throws IOException {
+        input();
+        solution();
+    }
+}
