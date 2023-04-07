2a8a642366d3792dcb5b0e3f142fd03f4585d9d4
KB Kim <jimi567@kookmin.ac.kr>
2023-04-07 23:36:31 +0900

    T : x

--- 실전프로젝트/공장 대여(demo).cpp
+++ 실전프로젝트/공장 대여(demo).cpp
@@ -68,7 +68,7 @@ int main(void) {
     }
 }
 
-//v2 - 시간 살짝? 개선
+//v2 - 정답 코드 f를 gcd로 고정시키는게 아니라 f의 후보군중 gcd의 약수도 포함될 수 있음을 생각해야됨
 #include <bits/stdc++.h>
 using namespace std;
 #define X first
@@ -81,21 +81,23 @@ int gcd(int a, int b) {
     if (b == 0) return a;
     return gcd(b, a % b);
 }
-int get_f(vector<int> v) {
+vector<int> get_f(vector<int> v) {
     int tmp = 0;
-    for (int i = 0;i < v.size()-1;i++) {
+    for (int i = 0;i < v.size() - 1;i++) {
         if (!tmp) tmp = gcd(v[i], v[i + 1]);
         else {
-            tmp = gcd(tmp, v[i+1]);
+            tmp = gcd(tmp, v[i + 1]);
         }
     }
-    return tmp;
+    vector<int> ret;
+    for (int i = 1;i <= tmp;i++) if (tmp % i == 0) ret.push_back(i);
+    return ret;
 }
 int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cin >> tc;
-    
+
     while (tc--) {
         cin >> n >> t;
         vector<pair<int, int>> info;
@@ -106,54 +108,37 @@ int main(void) {
             info.push_back({ time,price });
             arg.push_back(price);
         }
-        int f = get_f(arg);
+        vector<int> f = get_f(arg); //f의 후보군
         sort(info.begin(), info.end());
-        int point;
-        for (int i = 0;i < info.size();i++) {
-            if (info[i].Y > f) {
-                point = info[i].X;
-                break;
-            }
-        }
-        int st = 1;
-        for (int i = 1;i < point;i++) {
-            bool can = true;
+        vector<pair<int, int>> ct; //가능합 조합이 들어가는 벡터
+        for (int i = 0;i < f.size();i++) {
+            int point = 0;
             for (int j = 0;j < info.size();j++) {
-                int cost = (info[j].X / i + 1) * f;
-                if (cost != info[j].Y) {
-                    can = false;
+                if (info[j].Y > f[i]) {
+                    point = info[j].X;
                     break;
                 }
             }
-            if (can) {
-                st = i;
-                break;
-            }
-        }
-        int en = st;
-        for (int i = en; i < point;i++) {
-            bool can = true;
-            for (int j = 0;j < info.size();j++) {
-                int cost = (info[j].X / i + 1) * f;
-                if (cost != info[j].Y) {
+            for (int j = 1;j <= point;j++) {
+                bool can = true;
+                for (int k = 0;k < info.size();k++) {
+                    int cost = ((info[k].X / j) + 1) * f[i];
+                    if (cost == info[k].Y) continue;
                     can = false;
                     break;
                 }
-            }
-            if (!can) {
-                en = i - 1;
-                break;
+                if (can) ct.push_back({j,f[i]});
             }
         }
-
-        int mn = 0x7f7f7f7f;
-        int mx = 0;
-        for (int i = st;i <= en;i++) {
-            mx = max(mx, (t / i + 1) * f);
-            mn = min(mn, (t / i + 1) * f );
+           //int overflow 항상 생각하자
+        ll mn = 0x10f10f10f10f;
+        ll mx = 0;
+        for (int i = 0;i < ct.size();i++) {
+            mx = max(mx, (ll)(t / ct[i].X + 1) * ct[i].Y);
+            mn = min(mn, (ll)(t / ct[i].X + 1) * ct[i].Y);
         }
-        if (mx == 0) cout << -1 << '\n';
+        if (!ct.size()) cout << -1 << '\n';
         else cout << mn << " " << mx << '\n';
-        
+
     }
 }
