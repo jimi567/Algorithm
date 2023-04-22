579ed3db61fdf7726c6970bf6e22e9beb4083a96
KB Kim <jimi567@kookmin.ac.kr>
2023-04-22 23:53:51 +0900

    T : o

---
+++ Baek Joon백준/백트래킹/15664 - N과 M(10).cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define fastIO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define endl '\n'
+#define X first
+#define Y second
+int n, k;
+int arr[10];
+bool used[10];
+unordered_map<string, bool> dic;
+vector<int> v;
+void go() {
+	if (v.size() == k) {
+		string st;
+		for (int i : v) {
+			st += to_string(i) + " ";
+		}
+		if (dic.find(st) != dic.end()) return;
+		cout << st << endl;
+		dic.insert({ st,true });
+		return;
+	}
+
+	for (int i = 0;i < n;i++) {
+		if (used[i] ||(v.size() != 0&& v.back() > arr[i]))continue;
+		used[i] = 1;
+		v.push_back(arr[i]);
+		go();
+		v.pop_back();
+		used[i] = 0;
+	}
+}
+
+int main(void) {
+	fastIO;
+	cin >> n >> k;
+	for(int i=0;i<n;i++) cin >> arr[i];
+	sort(arr, arr + n);
+	go();
+}
