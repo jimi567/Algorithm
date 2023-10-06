3c5c3c87d6bf107a08473ef33ba9e932e13ab5eb
KB Kim <jimi567@kookmin.ac.kr>
2023-10-06 13:48:37 +0900

    Update 보물찾기.cpp

--- 실전프로젝트/보물찾기.cpp
+++ 실전프로젝트/보물찾기.cpp
@@ -64,3 +64,60 @@ int main(void) {
 		cout << x << " " << y << endl;
 	}
 }
+//2트
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define fastIO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define endl '\n'
+#define X first
+#define Y second
+
+int tc;
+//f r b l
+int dx[4] = { 1,0,-1,0 };
+int dy[4] = { 0,1,0,-1 };
+
+int n;
+string arr[55][55];
+bool chk[55][55][4];
+
+int parseDir(char c) {
+    if (c == 'F') return 0;
+    if (c == 'R') return 1;
+    if (c == 'B') return 2;
+    return 3;
+}
+int main(void) {
+    fastIO;
+    
+   
+    cin >> tc;
+    while (tc--) {
+
+        cin >> n;
+        for (int i = 0;i < n;i++)
+            for (int j = 0;j < n;j++) cin >> arr[i][j];
+
+        memset(chk, false, sizeof(chk));
+
+;       int dir = 2;
+        
+        pair<int, int> cur = { 0,0 };
+        while (!chk[cur.X][cur.Y][dir]) {
+            chk[cur.X][cur.Y][dir] = 1;
+            dir += parseDir(arr[cur.X][cur.Y][0]);
+            dir %= 4;
+            int d = arr[cur.X][cur.Y][1] - '0';
+            
+            int nx = cur.X + dx[dir] * d;
+            int ny = cur.Y + dy[dir] * d;
+            cur = { nx,ny };
+            
+        }
+
+        cout << cur.X << " " << cur.Y << endl;
+    }
+    
+    
+}
