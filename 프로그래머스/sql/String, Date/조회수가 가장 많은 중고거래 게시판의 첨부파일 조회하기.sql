a5fa9a1168c2b4c95f45325014438ef00a65a193
김기범(Kibum kim) <jimi567@kookmin.ac.kr>
2024-11-01 20:19:44 +0900

    T : o CONCAT

---
+++ 프로그래머스/sql/String, Date/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,8 @@
+SELECT CONCAT('/home/grep/src/',F.BOARD_ID,'/',F.FILE_ID,F.FILE_NAME,F.FILE_EXT) AS FILE_PATH
+FROM USED_GOODS_FILE AS F
+WHERE F.BOARD_ID = (SELECT BB.BOARD_ID
+                    FROM USED_GOODS_BOARD AS BB
+                    ORDER BY BB.VIEWS DESC
+                    LIMIT 1)
+                    
+ORDER BY F.FILE_ID DESC;
